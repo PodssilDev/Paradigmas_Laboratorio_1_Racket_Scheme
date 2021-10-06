e4d154d18f949d356f2d20f69692baadd48bf7db
DoopDev <john.serrano@usach.cl>
2021-10-06 00:45:14 -0300

    Se completan los selectores, modificadores y aspectos de documentación
    Se completan los selectores, modificadores y aspectos de documentación del TDA Paradigmas. Cambios podrían suceder si es que aparece la idea de alguna otra función para el TDA, o bien, si no son necesarios los ejemplos para los TDAs.

--- TDAParadigmadocs.rkt
+++ TDAParadigmadocs.rkt
@@ -38,9 +38,99 @@
   #f
   )
   )
+
 ; SELECTORES
+
+; Dominio: Un documento de tipo paradigmadocs
+; Recorrido: String
+; Descripcion: Obtiene el nombre del documento en formato string
+; Tipo de recursion: No se utiliza recursion
+(define (getNombre docs)
+  (if (isParadigmadocs? docs)
+      (car docs)
+      null)
+  )
+
+; Dominio: Un documento de tipo paradigmadocs
+; Recorrido: Lista de Enteros
+; Descripcion: Obtiene la fecha de creacion del documento, en formato lista
+; Tipo de recursion: No se utiliza recursion
+(define (getFecha docs)
+  (if (isParadigmadocs? docs)
+      (car (cdr docs))
+      null)
+  )
+
+; Dominio: Un documento de tipo paradigmadocs
+; Recorrido: Un texto (String)
+; Descripcion: Obtiene el texto guardado en la posicion de encryptFn
+; Tipo de recursion: No se utiliza recursion
+(define (getEncrypt docs)
+  (if (isParadigmadocs? docs)
+      (car (cdr (cdr docs)))
+      null)
+  )
+
+; Dominio: Un documento de tipo paradigmadocs
+; Recorrido: Un texto (String)
+; Descripcion: Obtiene el texto guardado en la posicion de decryptFn
+; Tipo de recursion: No se utiliza recursion
+(define (getDecrypt docs)
+  (if (isParadigmadocs? docs)
+      (car (cdr (cdr (cdr docs))))
+      null)
+  )
+
 ; MODIFICADORES
+
+; Dominio: Un documento de tipo paradigmadocs y un string
+; Recorrido: Una lista (de tipo paradigmadocs)
+; Descripcion: Se modifica el nombre del documento. Si el nombre es invalido, se entrega la lista sin modificaciones
+; Tipo de recursion: No se utiliza recursion
+(define (setNombre docs newNombre)
+  (if (and (isParadigmadocs? docs)(string? newNombre))
+      (paradigmadocs newNombre (getFecha docs) (getEncrypt docs) (getDecrypt docs))
+      docs))
+
+; Dominio: Un doumento de tipo paradigmadocs y una lista de enteros
+; Recorrido: Una lista (de tipo paradigmadocs)
+; Descripcion: Se modifica la fecha del documento. Si la fecha es invalida, se entrega la lista sin modificaciones
+; Tipo de recursion: No se utiliza recursion
+(define (setFecha docs newFecha)
+  (if (and(and(isParadigmadocs? docs)(list? newFecha))(not(empty? newFecha)))
+      (paradigmadocs (getNombre docs) newFecha (getEncrypt docs) (getDecrypt docs))
+      docs))
+
+; Dominio: Un documento de tipo paradigmadocs y un string
+; Recorrido: Una lista (de tipo paradigmadocs)
+; Descripcion: Se modifica el texto en la posicion de encryptFn. Si el texto es invalido, se entrega la lista sin modificaciones
+; Tipo de recursion: No se utiliza recursion
+(define (setEncrypt docs newEncrypt)
+  (if (and (isParadigmadocs? docs)(string? newEncrypt))
+      (paradigmadocs (getNombre docs) (getFecha docs) newEncrypt (getDecrypt docs))
+      docs))
+
+; Dominio: Un documento de tipo paradigmadocs y un string
+; Recorrido: Una lista (de tipo paradigmadocs)
+; Descripcion: Se modifica el texto en la posicion de decryptFn. Si el texto es invalido, se entrega la lista sin modificaciones
+; Tipo de recursion: No se utiliza recursion
+(define (setDecrypt docs newDecrypt)
+  (if (and (isParadigmadocs? docs)(string? newDecrypt))
+      (paradigmadocs (getNombre docs) (getFecha docs) (getEncrypt docs) newDecrypt)
+      docs))
+
 ; OTRAS FUNCIONES
+
+; No se tienen mas funciones extras.
+
 ; EJEMPLOS
-(define emptyGDocs (paradigmadocs "gDocs" (date 25 10 2021) encryptFn encryptFn))
+
+(define emptyGDocs (paradigmadocs "gDocs" (date 25 10 2021) encryptFn decryptFn)) 
 (define pertenencia1 (isParadigmadocs? emptyGDocs))
+(define nombre (getNombre emptyGDocs))
+(define fecha (getFecha emptyGDocs))
+(define mensaje (getDecrypt emptyGDocs))
+(define cambio (setNombre emptyGDocs 1233)) ; Ejemplo erroneo
+(define fechacambio (setFecha emptyGDocs (date 'hi' 10 2021)))
+(define string(setEncrypt emptyGDocs "Un nuevo mensaje para el dia"))
+(define string2(setDecrypt emptyGDocs "Vamos que se puede"))
